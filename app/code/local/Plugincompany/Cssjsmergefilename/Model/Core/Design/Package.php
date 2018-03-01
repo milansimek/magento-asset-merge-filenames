485f8a33e7a2ef11c410ae5afa57b107a4126c12
Milan <milan@plugin.company>
2018-03-01 03:11:37 +0100

    initial commit

---
+++ app/code/local/Plugincompany/Cssjsmergefilename/Model/Core/Design/Package.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by:  Milan Simek
+ * Company:     Plugin Company
+ *
+ * LICENSE: http://plugin.company/docs/magento-extensions/magento-extension-license-agreement
+ *
+ * YOU WILL ALSO FIND A PDF COPY OF THE LICENSE IN THE DOWNLOADED ZIP FILE
+ *
+ * FOR QUESTIONS AND SUPPORT
+ * PLEASE DON'T HESITATE TO CONTACT US AT:
+ *
+ * SUPPORT@PLUGIN.COMPANY
+ */ 
+class Plugincompany_Cssjsmergefilename_Model_Core_Design_Package extends Mage_Core_Model_Design_Package
+{
+    /**
+     * Merge specified css files and return URL to the merged file on success
+     *
+     * @param $files
+     * @return string
+     */
+    public function getMergedCssUrl($files)
+    {
+        // secure or unsecure
+        $isSecure = Mage::app()->getRequest()->isSecure();
+        $mergerDir = $isSecure ? 'css_secure' : 'css';
+        $targetDir = $this->_initMergerDir($mergerDir);
+        if (!$targetDir) {
+            return '';
+        }
+
+        // base hostname & port
+        $baseMediaUrl = Mage::getBaseUrl('media', $isSecure);
+        $hostname = parse_url($baseMediaUrl, PHP_URL_HOST);
+        $port = parse_url($baseMediaUrl, PHP_URL_PORT);
+        if (false === $port) {
+            $port = $isSecure ? 443 : 80;
+        }
+
+        // merge into target file
+        $targetFilename = md5($this->getMd5ForFiles($files) . "|{$hostname}|{$port}") . '.css';
+
+        $mergeFilesResult = $this->_mergeFiles(
+            $files, $targetDir . DS . $targetFilename,
+            false,
+            array($this, 'beforeMergeCss'),
+            'css'
+        );
+
+        if ($mergeFilesResult) {
+            return $baseMediaUrl . $mergerDir . '/' . $targetFilename;
+        }
+        return '';
+    }
+    /**
+     * Merge specified javascript files and return URL to the merged file on success
+     *
+     * @param $files
+     * @return string
+     */
+    public function getMergedJsUrl($files)
+    {
+        $targetFilename = $this->getMd5ForFiles($files) . '.js';
+        $targetDir = $this->_initMergerDir('js');
+        if (!$targetDir) {
+            return '';
+        }
+        if ($this->_mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
+            return Mage::getBaseUrl('media', Mage::app()->getRequest()->isSecure()) . 'js/' . $targetFilename;
+        }
+        return '';
+    }
+
+    private function getMd5ForFiles($files)
+    {
+        $md5 = '';
+        foreach($files as $filePath){
+            $md5 .= md5_file($filePath);
+        }
+        return md5($md5);
+    }
+
+}
