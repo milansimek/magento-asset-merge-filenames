0b7524515dfe6be9e1487e379f5f81cf8519e042
Milan <milan@plugin.company>
2018-03-01 03:18:00 +0100

    removed license

--- app/code/local/Plugincompany/Cssjsmergefilename/Model/Core/Design/Package.php
+++ app/code/local/Plugincompany/Cssjsmergefilename/Model/Core/Design/Package.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Created by:  Milan Simek
- * Company:     Plugin Company
- *
- * LICENSE: http://plugin.company/docs/magento-extensions/magento-extension-license-agreement
- *
- * YOU WILL ALSO FIND A PDF COPY OF THE LICENSE IN THE DOWNLOADED ZIP FILE
- *
- * FOR QUESTIONS AND SUPPORT
- * PLEASE DON'T HESITATE TO CONTACT US AT:
- *
- * SUPPORT@PLUGIN.COMPANY
- */ 
 class Plugincompany_Cssjsmergefilename_Model_Core_Design_Package extends Mage_Core_Model_Design_Package
 {
     /**
